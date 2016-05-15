f9635436913473cb013716d17bc2e00d7e63b961
Chris Calderon <calderon.christian760@gmail.com>
2016-05-14 21:48:40 -0700

    made interface more straight forward, updated tests.

--- test.py
+++ test.py
@@ -5,15 +5,16 @@ def main():
     a1 = myTest.accounts[0]
     a2 = myTest.accounts[1]
     a2Send = {'sender':a2.privkey}
-    test_cases = [('addToRegistry', ('foo', 123, 0), {}, 1),
-                  ('addToRegistry', ('foo', 456, 0), a2Send, 1),
-                  ('getVal', (a1.address_as_int, 'foo'), {}, 123),
-                  ('getVal', (a2.address_as_int, 'foo'), a2Send, 456),
-                  ('getOwnVal', ('foo',), {}, 123),
-                  ('addToRegistry', ('foo', 789, 0), {}, 0),
-                  ('getOwnVal', ('foo',), {}, 123),
-                  ('addToRegistry', ('foo', 789, 1), {}, 1),
-                  ('getOwnVal', ('foo',), {}, 789)]
+    test_cases = [('register', ('foo', 123), {}, 1),
+                  ('register', ('foo', 456), a2Send, 1),
+                  ('get_val', (a1.address_as_int, 'foo'), {}, 123),
+                  ('get_val', (a2.address_as_int, 'foo'), a2Send, 456),
+                  ('get_own_val', ('foo',), {}, 123),
+                  ('register', ('foo', 789), {}, 0),
+                  ('get_own_val', ('foo',), {}, 123),
+                  ('update', ('foo', 789), {}, 1),
+                  ('get_own_val', ('foo',), {}, 789),
+                  ('unregister', ('foo',), {}, 1)]
     myTest.run_tests(test_cases)
 
 if __name__ == '__main__':
