19fd7d279146b7c49a2828bfbfd43d099793cdbe
Chris Calderon <calderon.christian760@gmail.com>
2016-05-16 22:37:11 -0700

    Slight refactor; added ability to change many keys at once

--- test.py
+++ test.py
@@ -7,14 +7,20 @@ def main():
     a2Send = {'sender':a2.privkey}
     test_cases = [('register', ('foo', 123), {}, 1),
                   ('register', ('foo', 456), a2Send, 1),
-                  ('get_val', (a1.address_as_int, 'foo'), {}, 123),
-                  ('get_val', (a2.address_as_int, 'foo'), a2Send, 456),
-                  ('get_own_val', ('foo',), {}, 123),
+                  ('lookup', (a1.address_as_int, 'foo'), {}, 123),
+                  ('lookup', (a2.address_as_int, 'foo'), a2Send, 456),
+                  ('my_lookup', ('foo',), {}, 123),
                   ('register', ('foo', 789), {}, 0),
-                  ('get_own_val', ('foo',), {}, 123),
+                  ('my_lookup', ('foo',), {}, 123),
                   ('update', ('foo', 789), {}, 1),
-                  ('get_own_val', ('foo',), {}, 789),
-                  ('unregister', ('foo',), {}, 1)]
+                  ('my_lookup', ('foo',), {}, 789),
+                  ('remove', ('foo',), {}, 1),
+                  ('multiregister', (['bar', 'baz'], [100, 200]), {}, [1, 0, 0]),
+                  # foo was removed earlier
+                  ('multiremove', (['foo', 'baz'],), {}, [0, int('foo'.encode('hex'), 16)]),
+                  ('my_lookup', ('baz',), {}, 200),
+                  ('multiupdate', (['bar', 'baz'], [110, 210]), {}, [1, 0, 0])]
+
     myTest.run_tests(test_cases)
 
 if __name__ == '__main__':
