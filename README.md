cee1be98ca8f27b6e44c0b4bc1a20908721ff697
Chris Calderon <calderon.christian760@gmail.com>
2016-05-24 14:13:24 -0700

    Made registry ownership transferable, new tests to come!

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Registry
-A Serpent contract that lets addresses own a key -> address mapping.
+A contract for making registries which are owned,
+with transferable ownership.
+
+## Basic rules of the contract:
+
+1. All registries are identified by a natural (i.e. positive) number (called it's ID or reg_id).
+
+2. Every registry has an owner, who can mutate it's entries.
+
+3. Every owner has one default registry that can be used for
+convenient operations.
+
+4. Every registry can be used for lookups if you know it's ID.
+
+## How stored data is structured:
+
+  sload(reg_id) # The owner of the registry.
+
+  sload(sha3([reg_id, key]:arr)) # The value accosicated with "key" in the registry.
+
+  sload(msg.sender) # The default registry to use for this owner's convenience functions
+
--- test.py
+++ test.py
@@ -5,7 +5,9 @@ def main():
     a1 = myTest.accounts[0]
     a2 = myTest.accounts[1]
     a2Send = {'sender':a2.privkey}
-    test_cases = [('register', ('foo', 123), {}, 1),
+    test_cases = [('create_registry', (), {}, 1),
+                  ('create_registry', (), a2Send, 2),
+                  ('register', ('foo', 123), {}, 1),
                   ('register', ('foo', 456), a2Send, 1),
                   ('lookup', (a1.address_as_int, 'foo'), {}, 123),
                   ('lookup', (a2.address_as_int, 'foo'), a2Send, 456),
@@ -14,12 +16,7 @@ def main():
                   ('my_lookup', ('foo',), {}, 123),
                   ('update', ('foo', 789), {}, 1),
                   ('my_lookup', ('foo',), {}, 789),
-                  ('remove', ('foo',), {}, 1),
-                  ('multiregister', (['bar', 'baz'], [100, 200]), {}, [1, 0, 0]),
-                  # foo was removed earlier
-                  ('multiremove', (['foo', 'baz'],), {}, [0, int('foo'.encode('hex'), 16)]),
-                  ('my_lookup', ('baz',), {}, 200),
-                  ('multiupdate', (['bar', 'baz'], [110, 210]), {}, [1, 0, 0])]
+                  ('remove', ('foo',), {}, 1)]
 
     myTest.run_tests(test_cases)
 
