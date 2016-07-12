b4691c133a0b6623dc9df34d9615771f34204fe8
Chris Calderon <calderon.christian760@gmail.com>
2016-07-11 22:09:21 -0700

    Finished updating tests, finalized version of registry.se

--- test.py
+++ test.py
@@ -1,37 +1,47 @@
-from serpent_tests import ContractTester, Accounts, Assert
+from serpent_tests import ContractTest, Accounts
 
-class TestRegistry:
-    def test(self):
-        myTest = ContractTester('registry.se')
-        # Use a different sender sometimes to show the ownership stuff.
-        for i, account in enumerate(Accounts):
-            sender = {'sender':account.prikvey}
-            myTest.create_registry(kwds=sender, expects=(i+1))
-        
-        for i, account in enumerate(Accounts):
-            pass
-        myTest.register(args=('foo', 123), expects=1)
-        myTest.register(args=('foo', 456), expects=1, kwds=a1send)
-        myTest.lookup(args=(a0.as_int, 'foo'), expects=123)
-        myTest.lookup(args=(a1.as_int, 'foo'), expects=456)
-        myTest.update(args=('foo', 789), expects=1)
-        myTest.update(args=('bar', 1337), expects=0)
-'''
-    test_cases = [('create_registry', (), {}, 1),
-                  ('create_registry', (), a2Send, 2),
-                  ('register', ('foo', 123), {}, 1),
-                  ('register', ('foo', 456), a2Send, 1),
-                  ('lookup', (a1.address_as_int, 'foo'), {}, 123),
-                  ('lookup', (a2.address_as_int, 'foo'), a2Send, 456),
-                  ('my_lookup', ('foo',), {}, 123),
-                  ('register', ('foo', 789), {}, 0),
-                  ('my_lookup', ('foo',), {}, 123),
-                  ('update', ('foo', 789), {}, 1),
-                  ('my_lookup', ('foo',), {}, 789),
-                  ('remove', ('foo',), {}, 1)]
 
-    myTest.run_tests(test_cases)
+def test_registry():
+    myTest = ContractTest('registry.se')
+    # Use a different sender sometimes to show the ownership stuff.
+    for i, account in enumerate(Accounts[:3]):
+        reg_id = i + 1
+        foo_val = 2**(i+1)
+        bar_val = 3**(i+1)
+        kwds = {'sender':account.privkey}
+        myTest.create_registry(kwds=kwds, compare=reg_id) # creates a registry for the account
+        myTest.get_owner(args=(reg_id,), compare=account.as_int, kwds=kwds) # the registry is owned by the account
+        myTest.get_default_registry(args=(account.as_int,), compare=reg_id, kwds=kwds) # the account's default registry
+        myTest.register(args=('foo', foo_val), compare=1, kwds=kwds) # adds a new key
+        myTest.register(args=('foo', foo_val - 1), compare=0, kwds=kwds)  # can't update with register
+        myTest.register_by_id(args=(reg_id, 'bar', bar_val), compare=1, kwds=kwds) # registers using id
+        myTest.register_by_id(args=(reg_id, 'bar', bar_val - 1), compare=0, kwds=kwds) # can't update
+        myTest.lookup(args=(account.as_int, 'foo'), compare=foo_val, kwds=kwds) # looks up the val in foo
+        myTest.lookup_by_id(args=(reg_id, 'bar'), compare=bar_val, kwds=kwds) # looks up the val in bar
+        myTest.update(args=('foo', foo_val - 1), compare=1, kwds=kwds) # updates foo
+        myTest.update(args=('foo1', foo_val), compare=0, kwds=kwds)  #can't register with update
+        myTest.update_by_id(args=(reg_id, 'bar', bar_val - 1), compare=1, kwds=kwds) # updates bar
+        myTest.update_by_id(args=(reg_id, 'bar1', bar_val), compare=0, kwds=kwds) # can't register here either
+        myTest.state.mine(1, coinbase=account.address)
 
-if __name__ == '__main__':
-    main()
-'''
+    a0, a1, a2 = Accounts[:3]
+    a0_send = {'sender': a0.privkey}
+    a1_send = {'sender': a1.privkey}
+    a2_send = {'sender': a2.privkey}
+    myTest.transfer_owner(args=(a0.as_int, 2), compare=1, kwds=a1_send)  #a1 transfered reg_id 2 to a0
+    myTest.get_default_registry(args=(a1.as_int,), compare=0) #a1 no longer has reg2 as default
+    myTest.get_owner(args=(2,), compare=a0.as_int) #a0 now owns reg 2
+    myTest.lookup(args=(a0.as_int, 'foo'), compare=1) #a0 still uses reg 1 by default
+    myTest.set_default_registry(args=(2,), compare=1, kwds=a0_send) #set new default to reg 2
+    myTest.lookup(args=(a0.as_int, 'foo'), compare=3) #now a0 uses reg 2
+    myTest.remove(args=('foo',), compare=1, kwds=a0_send) #a0 removes foo from reg 2
+    myTest.lookup(args=('foo',), compare=0) #lookup returns 0
+    myTest.lookup_by_id(args=(2, 'bar'), compare=8)
+    myTest.remove_by_id(args=(2, 'bar'), compare=1, kwds=a0_send) # a0 removes bar from reg 2
+    myTest.lookup_by_id(args=(2, 'bar'), compare=0) # foo removed from reg 1, returns 0
+    myTest.remove_by_id2(args=(1, ['foo', 'bar']), compare=[1, 2], kwds=a0_send) # a0 removes foo and bar from reg 1
+    myTest.lookup_by_id(args=(1, 'foo'), compare=0) # lookup now returns 0
+    myTest.remove2(args=(['foo', 'bar'],), compare=[1, 2], kwds=a2_send) # a2 removes foo and bar from reg 3
+    myTest.lookup_by_id(args=(3, 'foo'), compare=0)
+    myTest.lookup_by_id(args=(3, 'bar'), compare=0)
+
